d5e63d9233f008975cab65268fcf392a6806ed74
Safwanrockz <safini99@yahoo.com>
2016-09-01 18:04:02 +0300

    Edited dashboard file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <table class="table table-hover table-striped">
+            <thead>
+                <th>Task ID</th><th>Title</th><th>Description</th><th>Progress</th><th>Options</th>
+            </thead>
+            <tbody>
+                <tr><td>0</td><td>Test Title</td><td>Complete this test title</td><td><span class="label label-success">Done</span></td>
+                <td><button type="btn btn-primary"><i class="fa fa-cog"></i></button></td></tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
