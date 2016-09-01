db23eec5f394fcb9abecb09f857789084bdada1e
Safwanrockz <safini99@yahoo.com>
2016-09-01 17:13:16 +0300

    Modification of the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Test 
                 </div>
 
                 <div class="links">
