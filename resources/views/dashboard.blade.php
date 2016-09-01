b600ee32074601620b7d3a010c2c81980e3407f6
Safwanrockz <safini99@yahoo.com>
2016-09-01 18:36:56 +0300

    Added task addition form for admins

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,12 +4,47 @@
     <div class="container">
         <table class="table table-hover table-striped">
             <thead>
-                <th>Task ID</th><th>Title</th><th>Description</th><th>Progress</th><th>Options</th>
+            <th>Task ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Progress</th>
+            <th>Options</th>
             </thead>
             <tbody>
-                <tr><td>0</td><td>Test Title</td><td>Complete this test title</td><td><span class="label label-success">Done</span></td>
-                <td><button type="btn btn-primary"><i class="fa fa-cog"></i></button></td></tr>
+            @foreach($tasks as $task)
+                <tr>
+                    <td>$task->id</td>
+                    <td>$task->title</td>
+                    <td>$task->content</td>
+                    <td><span class="label label-success">$task->isDone</span></td>
+                    <td>
+                        <button type="btn btn-primary"><i class="fa fa-cog"></i></button>
+                    </td>
+                </tr>
+
+            @endforeach
             </tbody>
         </table>
+        @if(Auth::user()->isAdmin)
+            <div class="panel panel-default">
+                <div class="panel-heading">Add task</div>
+                <div class="panel-body">
+                    <form>
+                        <div class="form-group">
+                        <table>
+                            <tr><td>To user: </td><td><select class="form-control">
+                                        @foreach($users as $user)
+                                            <option>$user->name</option>
+                                        @endforeach
+                                    </select></td></tr>
+                            <tr><td>Task title: </td><td><input class="form-control" type="text"></td></tr>
+                            <tr><td>Description: </td><td><textarea class="form-control"></textarea></td></tr>
+                            <tr><td><button type="btn btn-primary"><i class="fa fa-send"></i> Send</button></td></tr>
+                        </table>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
